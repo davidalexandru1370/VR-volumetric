d1265b2ee2698ef73389d4665ecc6a8499e80aac
David Alexandru <fanefaraon696@gmail.com>
2023-11-14 16:15:32 +0200

    added: volumetric rendenring

---
+++ Ellipsoid.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+
+namespace rt
+{
+    public class Ellipsoid : Geometry
+    {
+        private Vector Center { get; }
+        private Vector SemiAxesLength { get; }
+        private double Radius { get; }
+        
+        
+        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            SemiAxesLength = semiAxesLength;
+            Radius = radius;
+        }
+
+        public Ellipsoid(Vector center, Vector semiAxesLength, double radius, Color color) : base(color)
+        {
+            Center = center;
+            SemiAxesLength = semiAxesLength;
+            Radius = radius;
+        }
+
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            double semiAxesXSquared = SemiAxesLength.X * SemiAxesLength.X;
+            double semiAxesYSquared = SemiAxesLength.Y * SemiAxesLength.Y;
+            double semiAxesZSquared = SemiAxesLength.Z * SemiAxesLength.Z;
+
+            //double a = line.Dx * line.Dx;
+            double a = line.Dx.X * line.Dx.X / semiAxesXSquared +
+                       line.Dx.Y * line.Dx.Y / semiAxesYSquared +
+                       line.Dx.Z * line.Dx.Z / semiAxesZSquared;
+
+            //double b = 2 * ((line.X0 * line.Dx) - (line.Dx * Center));
+            double b = 2 *
+                ((line.Dx.X * (line.X0.X - Center.X)) / semiAxesXSquared +
+                (line.Dx.Y * (line.X0.Y - Center.Y)) / semiAxesYSquared +
+                (line.Dx.Z * (line.X0.Z - Center.Z)) / semiAxesZSquared);
+
+            //double c = (line.X0 * line.X0 + Center * Center)  - Radius * Radius - (line.X0 * Center) * 2;
+            double c = ((line.X0.X * line.X0.X) + (Center.X * Center.X)) / semiAxesXSquared +
+                       ((line.X0.Y * line.X0.Y) + (Center.Y * Center.Y)) / semiAxesYSquared +
+                       ((line.X0.Z * line.X0.Z) + (Center.Z * Center.Z)) / semiAxesZSquared +
+                        -2 * (
+                        line.X0.X * Center.X / semiAxesXSquared +
+                        line.X0.Y * Center.Y / semiAxesYSquared +
+                        line.X0.Z * Center.Z / semiAxesZSquared
+                        )
+                       - Radius * Radius;
+
+            double delta = b * b - 4.0f * a * c;
+            double epsilon = 0.0001;
+            Vector intersectionPoint;
+
+            Vector normal = new();
+
+            if (delta <= epsilon)
+            {
+                normal = new Vector(0, 0, 0);
+                return new Intersection(false, false, this, line, 0, null, Material, Color);
+            }
+
+            var (t1, t2) = ComputeSolutionsForSecondDegreeEquation(delta, a, b);
+
+            bool isT1Valid = t1 >= minDist && t2 <= maxDist;
+            bool isT2Valid = t2 >= minDist && t2 <= maxDist;
+
+
+            if (isT1Valid == false && isT2Valid == false)
+            {
+                return new Intersection(false, false, this, line, 0, null, Material, Color);
+            }
+
+            else if (isT1Valid == true && isT2Valid == false)
+            {
+                intersectionPoint = line.X0 + line.Dx * t1;
+                normal = Normal(intersectionPoint);
+                return new Intersection(true, true, this, line, t1, normal, Material, Color);
+            }
+            else if (isT1Valid == false && isT2Valid == true)
+            {
+                intersectionPoint = line.X0 + line.Dx * t2;
+                normal = Normal(intersectionPoint);
+                return new Intersection(true, true, this, line, t2, normal, Material, Color);
+            }
+
+            double distanceFactorToShortestPoint = Math.Min(t1, t2);
+            intersectionPoint = line.X0 + line.Dx * distanceFactorToShortestPoint;
+            normal = Normal(intersectionPoint);
+            return new Intersection(true, true, this, line, distanceFactorToShortestPoint, normal, Material, Color);
+        }
+
+        private Tuple<double, double> ComputeSolutionsForSecondDegreeEquation(double delta, double a, double b)
+        {
+            double t1 = (-b - Math.Sqrt(delta)) / ((double)2.0 * a);
+            double t2 = (-b + Math.Sqrt(delta)) / ((double)2.0 * a);
+
+            return new Tuple<double, double>(t1, t2);
+        }
+
+        public Vector Normal(Vector position)
+        {
+            return new Vector(2 * (position.X - Center.X) / (SemiAxesLength.X * SemiAxesLength.X),
+               2 * (position.Y - Center.Y) / (SemiAxesLength.Y * SemiAxesLength.Y),
+               2 * (position.Z - Center.Z) / (SemiAxesLength.Z * SemiAxesLength.Z)).Normalize();
+        }
+    }
+}
---
+++ RayTracer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace rt
+{
+    class RayTracer
+    {
+        private Geometry[] _geometries;
+        private Light[] _lights;
+
+        public RayTracer(Geometry[] geometries, Light[] lights)
+        {
+            this._geometries = geometries;
+            this._lights = lights;
+        }
+
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            return -n * viewPlaneSize / imgSize + viewPlaneSize / 2;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = Intersection.NONE;
+
+            foreach (var geometry in _geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            // TODO: ADD CODE HERE
+            double epsilon = 0.001;
+
+            Line lineStartingFromLightPoint = new Line(light.Position, point);
+            var directionVector = light.Position - point;
+            Intersection intersection = FindFirstIntersection(lineStartingFromLightPoint, 0, directionVector.Length() - epsilon);
+
+            if (!intersection.Valid || !intersection.Visible)
+            {
+                return true;
+            }
+
+            var difference = intersection.T - (light.Position - point).Length();
+            //-epsilon < difference < epsilon
+            if (-epsilon < difference && difference < epsilon)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var background = new Color(0.2, 0.2, 0.2, 1.0);
+
+            var image = new Image(width, height);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    // TODO: ADD CODE HERE
+                    var pointOnView = camera.Position + camera.Direction * camera.ViewPlaneDistance +
+                                     (camera.Up ^ camera.Direction) * ImageToViewPlane(i, width, camera.ViewPlaneWidth) +
+                                     camera.Up * ImageToViewPlane(j, height, camera.ViewPlaneHeight);
+                    var ray = new Line(camera.Position, pointOnView);
+                    var intersection = FindFirstIntersection(ray, camera.FrontPlaneDistance, camera.BackPlaneDistance);
+                    if (intersection.Valid && intersection.Visible)
+                    {
+                        var color = new Color();
+                        var lightningColor = new Color();
+                        foreach (var light in _lights)
+                        {
+                            lightningColor += intersection.Geometry.Material.Ambient * light.Ambient;
+
+                            if (IsLit(intersection.Position, light))
+                            {
+                                var normal = intersection.Normal;
+                                var cameraVector = (camera.Position - intersection.Position).Normalize();
+                                var vectorFromLight = (light.Position - intersection.Position).Normalize();
+                                var reflectionVector = (normal * (normal * vectorFromLight) * 2 - vectorFromLight).Normalize();
+
+                                if (normal * vectorFromLight > 0)
+                                {
+                                    lightningColor += intersection.Geometry.Material.Diffuse * light.Diffuse * (normal * vectorFromLight);
+                                }
+
+                                if (cameraVector * reflectionVector > 0)
+                                {
+                                    lightningColor += intersection.Geometry.Material.Specular * light.Specular *
+                                        Math.Pow(cameraVector * reflectionVector, intersection.Geometry.Material.Shininess);
+                                }
+
+                                lightningColor *= light.Intensity;
+                            }
+                        }
+
+                        color += lightningColor;
+                        image.SetPixel(i, j, color);
+                    }
+                    else
+                    {
+                        image.SetPixel(i, j, background);
+                    }
+                }
+            }
+
+            image.Store(filename);
+        }
+    }
+}
